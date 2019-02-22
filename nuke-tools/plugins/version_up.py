a44677661d460a811da8ef2a836bfcafd0d62e47
Samuel Christopher Earnest <scearnes@phillip.cs.byu.edu>
2019-02-21 23:29:54 -0700

    Nuke Versioning

--- nuke-tools/plugins/version_up.py
+++ nuke-tools/plugins/version_up.py
@@ -3,21 +3,38 @@ import os
 
 def go():
 
+    nukeDir = os.environ['NUKE_TOOLS_DIR']
+    os.chdir(nukeDir)
 
+    if not os.path.exists("Shots"):
+        os.mkdir("Shots")
 
+    path = os.path.join(nukeDir, "Shots")
+    os.chdir(path)
     filepath = nuke.toNode("root").name()
 
-    if (filepath != "Root"):
+
+    if (filepath == "Root"):
         nuke.message("We need to save")
         nuke.scriptSave();
-
     else:
-        nukeDir = os.environ['NUKE_TOOLS_DIR']
-        path = os.path.join(nukeDir, "versions")
+        if not os.path.exists("Versions"):
+            os.mkdir("Versions")
+
+        shotName = os.path.basename(filepath)
+        shotName = shotName[:-3]
+        path = os.path.join(path, "Versions/")
         os.chdir(path)
 
-        if os.path.exists("shot_01"):
+        x = 0;
+        found = False
+        while not found:
+            x += 1
+            shot = shotName + "_v" + str(x) + ".nk"
+            if not os.path.exists(shot):
+                found = True
+                break
 
-            nuke.message(filepath)
-        else:
-            nuke.message(os.getcwd())
+
+
+        nuke.scriptSave(path + shot);
