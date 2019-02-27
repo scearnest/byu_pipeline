6618b793ca7716a5a9b989be5b7ab74ff395c119
mswindler <matthew@ionixdesign.com>
2019-02-27 15:57:15 -0700

    add more tools

---
+++ tools/substance/tools.py
@@ -0,0 +1,21 @@
+from ... import tools
+
+class SubstanceTools(Tools):
+
+    __init__():
+        pass
+
+    @override
+    @classmethod
+    def checkout(self):
+        pass
+
+    @override
+    @classmethod
+    def publish(self):
+        pass
+
+    @override
+    @classmethod
+    def rollback(self):
+        pass
