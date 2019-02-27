9d58862bd0a03fd85b0561c6ad0a5a066a8c08b6
Rory Hutchings <rory.hutchings@gmail.com>
2019-02-27 15:26:49 -0700

    Skeleton classes for asset.py

--- asset_management/asset.py
+++ asset_management/asset.py
@@ -1,8 +1,48 @@
 
 
-class Asset():
+class Asset:
 
-    __init__(self, name):
+    def __init__(self, name, references, body_type, environment, file_path, pipeline_file):
         self.name = name
-
+        self.references = references
+        self.body_type = body_type
+        self.environment = environment
+        self.file_path = file_path
+        self.pipeline_file = pipeline_file
     
+    def default_departments(self):
+        return  # TODO stuff
+
+    def get_parent_directory(self):
+        return  # TODO stuff
+
+    def get_body_type(self):
+        return self.body_type
+
+    def get_element(self):
+        return  # TODO stuff
+
+    def create_element(self):
+        return  # TODO stuff
+
+    def list_elements(self):
+        return  # TODO stuff
+
+    def add_reference(self):
+        return  # TODO stuff
+
+    def remove_reference(self):
+        return  # TODO stuff
+
+    def set_description(self):
+        return  # TODO stuff
+
+    def get_references(self):
+        return  # TODO stuff
+
+    def has_relation(self):
+        return  # TODO stuff
+
+    def get_info(self):
+        return  # TODO stuff
+
